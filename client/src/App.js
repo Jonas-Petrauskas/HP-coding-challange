dd66ebf6278d2c25da053d332a8a4d90c6e77b3d
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2021-07-06 12:48:18 +0200

    add: live search box to filter added albums

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,21 @@ import { useState, useEffect } from 'react';
 function App() {
   
   const [albums, setAlbums] = useState([]);
+  const [filteredAlbums, setFilteredAlbums] = useState([]);
+
+  const searchAlbums = function ({target}) {
+    const temporatyFilteredAlbums = albums.filter(({collectionName}) => {
+      return collectionName.toLowerCase().includes(target.value.toLowerCase())
+    })
+    setFilteredAlbums(temporatyFilteredAlbums);
+  }
 
   useEffect(() => {
     fetch('http://localhost:3000/albums')
       .then((res) => res.json())
       .then((albums) => {
         setAlbums(albums)
+        setFilteredAlbums(albums)
       })
       .catch((err) => {
         console.log(err);
@@ -22,11 +31,12 @@ function App() {
             <input
               className="search"
               type="text" 
-              placeholder="Search album name.."  
+              placeholder="Search album name.."
+              onChange={searchAlbums}  
             />
           </div>
           <div className="albumsContainer">
-            {albums.map(({artistName, collectionName, artworkUrl60, id }) => {
+            {filteredAlbums.map(({artistName, collectionName, artworkUrl60, id }) => {
               return (
                 <div className="albumName" key={collectionName+id}>
                   <img src={artworkUrl60} alt={`Album cover for ${artistName} from their album ${collectionName}`}></img>
